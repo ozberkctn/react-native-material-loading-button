4678fc5a6bd70609abfab1a86dd9f61768e2900c
ozberkctn <ozberkctn@gmail.com>
2018-12-14 15:55:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ export default class MaterialButton extends PureComponent {
 
   render() {
     return (
-      <Ripple onPress={this.props.onPress} disabled={this.props.disabled || this.props.isLoading} rippleOpacity={1} rippleFades={false} rippleDuration={0}>
+      <Ripple  rippleColor={"#ffc853"} onPress={this.props.onPress} disabled={this.props.disabled || this.props.isLoading} rippleOpacity={1} rippleFades={false} rippleDuration={0}>
         <View style={this.getButtonStyle()}>
           {this.renderLoadingAnim()}
           {this.renderText()}
