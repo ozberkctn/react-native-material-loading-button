a32a85b5968f75fe94cde7b59247557fe1f72ac3
ozberkctn <ozberkctn@gmail.com>
2019-01-04 03:15:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-import React, { PureComponent } from 'react';
-import { TouchableOpacity, View, Text, StyleSheet, ActivityIndicator } from 'react-native';
-import Ripple from 'react-native-material-ripple';
-import PropTypes from 'prop-types';
-import styles from './styles';
+import React, { PureComponent } from "react";
+import {
+  TouchableOpacity,
+  View,
+  Text,
+  StyleSheet,
+  ActivityIndicator
+} from "react-native";
+import Ripple from "react-native-material-ripple";
+import PropTypes from "prop-types";
+import styles from "./styles";
 
 export default class MaterialButton extends PureComponent {
-  
   getButtonStyle() {
     let disabled = this.props.disabled || this.props.isLoading;
     let opacity = this.props.isLoading ? 0.6 : 0.3;
@@ -20,7 +25,12 @@ export default class MaterialButton extends PureComponent {
         return [styles.btn, this.props.style];
 
       case !this.props.flat && disabled:
-        return [styles.btn, styles.raised, { backgroundColor: this.props.color, opacity: opacity }, this.props.style];
+        return [
+          styles.btn,
+          styles.raised,
+          { backgroundColor: this.props.color, opacity: opacity },
+          this.props.style
+        ];
     }
   }
 
@@ -30,7 +40,11 @@ export default class MaterialButton extends PureComponent {
       defaultStyle.push(styles.loadingText);
     }
     if (this.props.flat) {
-      return [...defaultStyle, { color: this.props.color }, this.props.textStyle];
+      return [
+        ...defaultStyle,
+        { color: this.props.color },
+        this.props.textStyle
+      ];
     } else {
       return [...defaultStyle, this.props.textStyle];
     }
@@ -38,37 +52,43 @@ export default class MaterialButton extends PureComponent {
 
   renderLoadingAnim() {
     if (this.props.isLoading) {
-      let color = this.props.flat ? this.props.color : '#ffffff';
-      return <ActivityIndicator color={color} style={styles.loading}/>;
+      let color = this.props.flat ? this.props.color : "#ffffff";
+      return <ActivityIndicator color={color} style={styles.loading} />;
     }
     return null;
   }
 
   renderText() {
     if (this.props.isLoading) {
-      return <Text style={this.getTextStyle()}></Text>;
+      return <Text style={this.getTextStyle()} />;
     }
-    return <Text style={this.getTextStyle()}>{this.props.text.toUpperCase()}</Text>;
+    return (
+      <Text style={this.getTextStyle()}>{this.props.text.toUpperCase()}</Text>
+    );
   }
 
   render() {
     return (
-        <View style={this.getButtonStyle()}>
-          {this.renderLoadingAnim()}
-          {this.renderText()}
-        </View>
+      <View style={{ width: this.props.style.width, alignItems: "flex-end" }}>
+        <TouchableOpacity
+          onPress={this.props.onPress}
+          style={{ width: this.props.style.width }}
+        >
+          <View style={this.getButtonStyle()}>
+            {this.renderLoadingAnim()}
+            {this.renderText()}
+          </View>
+        </TouchableOpacity>
+      </View>
     );
   }
-
 }
-
-
 
 MaterialButton.defaultProps = {
   flat: false,
-  text: 'Button',
-  color: '#039be5',
+  text: "Button",
+  color: "#039be5",
   disabled: false,
   isLoading: false,
-  loadingText: ''
+  loadingText: ""
 };
